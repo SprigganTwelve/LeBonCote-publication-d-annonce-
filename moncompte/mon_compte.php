e76bfa93f2ea713d2e9da5f669d18d778f1af816
Paterne <angepaterne.dali@gmail.com>
2024-01-23 00:35:11 +0100

    .

--- moncompte/mon_compte.php
+++ moncompte/mon_compte.php
@@ -27,6 +27,7 @@ $row = mysqli_fetch_array($query);
     </h1>
     <span><a href="../recherche/recherche.php">RECHERCHE</a></span>
     <span><a href="update/update.php">METTRE A JOUR VOTR COMPTE ?</a></span>
+    <span><a href="../publication/publication.php">publication</a></span>
     <span><a href="logout.php">Se déconnecter</a></span>
 </body>
 
