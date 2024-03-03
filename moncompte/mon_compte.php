187c571554c165ed26cabd74cd9bd82ea09fa84e
Paterne <angepaterne.dali@gmail.com>
2024-03-03 04:03:23 +0100

    profile page has been constructed

--- moncompte/mon_compte.php
+++ moncompte/mon_compte.php
@@ -17,18 +17,95 @@ $row = mysqli_fetch_array($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Mon Compte</title>
 </head>
 
 <body>
-    <div>MON COMPTE</div>
-    <h1>
-        <?php echo $row["nom"] . " " . $row["prenom"] ?>
-    </h1>
-    <span><a href="../recherche/recherche.php">RECHERCHE</a></span>
-    <span><a href="update/update.php">METTRE A JOUR VOTR COMPTE ?</a></span>
-    <span><a href="../publication/publication.php">publication</a></span>
-    <span><a href="logout.php">Se déconnecter</a></span>
+
+
+    <a href="../home/index.php" class="logo"><span>LeBonCôté</span></a>
+    <ul class="changeDirectory">
+
+        <li><a href="../recherche/recherche.php">Mettre à jour mon profil</a></li>
+        <li><a href="../recherche/recherche.php">Recherche</a></li>
+        <li><a href="../recherche/recherche.php">Publier</a></li>
+    </ul>
+
+    <div class="container">
+
+        <h1 class="profileHead">Profile </h1>
+        <div class=" selfItems">
+
+            <div class='left'>
+                <img class="profile-avatar" src="../uploadAvatar/gojo-extension-du-territoire.jpg" alt="Avatar">
+                <strong class="title" style="color:white">
+                    bertrand
+                </strong>
+            </div>
+
+            <div class="infoAboutYou">
+
+                <strong class="title">Informmation</strong>
+
+                <div class='aboutYou'>
+                    <div>
+                        <p>
+                            Email</br>
+                        <h2>
+                            dddjj@jdh
+                        </h2>
+                        </p>
+                    </div>
+
+                    <div>
+                        <p>
+                            Phone</br>
+                        <h2>
+                            4255537
+                        </h2>
+                        </p>
+                    </div>
+                </div>
+
+                <strong>Projects</strong>
+
+                <div class="foot">
+                    <div>
+                        <strong>Recent</strong>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium corporis nostrum
+                            repellat
+                            aspernatur
+                            alias ad voluptas? Officia dolorum accusantium cum explicabo porro. Placeat aperiam in,
+                            doloribus
+                            blanditiis
+                            fugit labore rem.
+                        </p>
+                    </div>
+                    <div>
+                        <strong>Most viewed </strong>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium corporis nostrum
+                            repellat
+                            aspernatur
+                            alias ad voluptas? Officia dolorum accusantium cum explicabo porro. Placeat aperiam in,
+                            doloribus
+                            blanditiis
+                            fugit labore rem.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php
+
+    ?>
 </body>
 
 </html>
